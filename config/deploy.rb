6ea1a50fbd9d9bd1e48b9ad4b9650ee5be2967e5
Sami Haahtinen <sami@haahtinen.name>
2012-11-30 22:40:01 +0200

    Finalize deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,18 +12,46 @@ set :use_sudo, false
 set :copy_strategy, :export
 set :copy_cache, true
 
+# public/system is not needed for this
+set :shared_children, %w(log tmp/pids)
+
 server "web1.badwolf.fi", :web, :app
 
 require "rvm/capistrano"
 require "capistrano-unicorn"
 require "bundler/capistrano"
 
+namespace :aws do
+  desc <<-DESC
+    Copy credentials from local host to the server if the aws-credentials
+    file exists. The target destination is within the shared directory and
+    the file is then linked to the correct location.
+  DESC
+  task :credentials, :roles => ["app"] do
+    if File.exists?(File.join("config", "aws-credentials.rb"))
+      # Define file locations
+      shared_config_path = File.join(shared_path, "config")
+      shared_config = File.join(shared_config_path, "aws-credentials.rb")
+      aws_config = File.join(current_release, "config", "aws-credentials.rb")
+
+      # Create shared directory if it doesn't exist
+      run "#{try_sudo} mkdir -p #{shared_config_path}"
+
+      # Upload configuration to shared directory
+      upload(File.join("config","aws-credentials.rb"), shared_config)
+
+      # And finally link in to place
+      run "#{try_sudo} ln -fs #{shared_config} #{aws_config}"
+    end
+  end
+end
+
 # Install RVM before doing anything else, if not installed
 before 'deploy:setup', 'rvm:install_rvm'
 before 'deploy:setup', 'rvm:install_ruby'
 
-# Restart Unicorn
-after 'deploy:restart', 'unicorn:restart'
+# Install configuration before finalizing installation
+before 'deploy:finalize_update', 'aws:credentials'
 
-# if you want to clean up old releases on each deploy uncomment this:
+# There really is no need to keep old releases laying around
 after "deploy:restart", "deploy:cleanup"
