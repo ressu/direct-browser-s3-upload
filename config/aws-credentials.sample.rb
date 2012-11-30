71914357e2ba8638a84c317a1b93cfb52398220b
Sami Haahtinen <sami@haahtinen.name>
2012-11-30 23:15:15 +0200

    Fix configuration loading

--- config/aws-credentials.sample.rb
+++ config/aws-credentials.sample.rb
@@ -7,12 +7,12 @@
 
 # Content expiration time. This defines how long the content lives on the
 # server. (Seconds)
-# EXPIRE_TIME=3000
+#EXPIRE_TIME=3000
 
 # S3 URL. Default uses Ireland based S3
 #S3_URL='http://s3-ireland.amazonaws.com'
 
 # S3 credentials for uploading. If aws-credentials.rb file doesn't exist,
 # the values are read from the environment.
-# S3_KEY    = ""
-# S3_SECRET = ""
+#S3_KEY    = ""
+#S3_SECRET = ""
--- config/aws.rb
+++ config/aws.rb
@@ -1,15 +1,15 @@
 # AWS configuration, most of these settings should be overridden in
 # aws-credentials
-S3_BUCKET='/direct-upload'
-
-# EXPIRE_TIME=(60 * 5) # 5 minutes
-EXPIRE_TIME=3000
-S3_URL='http://s3-ireland.amazonaws.com'
-
 begin
   # Store AWS credentials in a file called aws-credentials.rb
-  require './aws-credentials'
+  require './config/aws-credentials'
 rescue LoadError
   S3_KEY    = ENV['S3_KEY']
   S3_SECRET = ENV['S3_SECRET']
 end
+
+S3_BUCKET='/direct-upload' unless defined?(S3_BUCKET)
+
+# EXPIRE_TIME=(60 * 5) # 5 minutes
+EXPIRE_TIME=3000 unless defined?(EXPIRE_TIME)
+S3_URL='http://s3-ireland.amazonaws.com' unless defined?(S3_URL)
