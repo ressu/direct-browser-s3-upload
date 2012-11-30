50eb0e923bb488ff784b05cc889baee571f7a48a
Sami Haahtinen <sami@haahtinen.name>
2012-11-30 21:43:00 +0200

    Document the settings in a sample file

---
+++ config/aws-credentials.sample.rb
@@ -0,0 +1,18 @@
+# These settings override the default configuration.
+#
+# Copy this file to aws-credentials.rb and adjust the settings accordingly
+
+# The Name of the bucket you wish to upload to
+#S3_BUCKET='/direct-upload'
+
+# Content expiration time. This defines how long the content lives on the
+# server. (Seconds)
+# EXPIRE_TIME=3000
+
+# S3 URL. Default uses Ireland based S3
+#S3_URL='http://s3-ireland.amazonaws.com'
+
+# S3 credentials for uploading. If aws-credentials.rb file doesn't exist,
+# the values are read from the environment.
+# S3_KEY    = ""
+# S3_SECRET = ""
--- config/aws.rb
+++ config/aws.rb
@@ -1,4 +1,10 @@
-# AWS configuration
+# AWS configuration, most of these settings should be overridden in
+# aws-credentials
+S3_BUCKET='/direct-upload'
+
+# EXPIRE_TIME=(60 * 5) # 5 minutes
+EXPIRE_TIME=3000
+S3_URL='http://s3-ireland.amazonaws.com'
 
 begin
   # Store AWS credentials in a file called aws-credentials.rb somewhere
@@ -8,8 +14,3 @@ rescue LoadError
   S3_KEY    = ENV['S3_KEY']
   S3_SECRET = ENV['S3_SECRET']
 end
-S3_BUCKET='/test-direct-upload'
-
-# EXPIRE_TIME=(60 * 5) # 5 minutes
-EXPIRE_TIME=3000
-S3_URL='http://s3-ireland.amazonaws.com'
