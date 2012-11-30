cb37c9fbebb798a6d5e9142f46a5a10626948968
Sami Haahtinen <sami@haahtinen.name>
2012-11-30 23:33:51 +0200

    Fix expiration description and default S3 host

--- config/aws-credentials.sample.rb
+++ config/aws-credentials.sample.rb
@@ -5,12 +5,11 @@
 # The Name of the bucket you wish to upload to
 #S3_BUCKET='/direct-upload'
 
-# Content expiration time. This defines how long the content lives on the
-# server. (Seconds)
+# Token expiration time. This defines how long the token is valid (Seconds)
 #EXPIRE_TIME=3000
 
 # S3 URL. Default uses Ireland based S3
-#S3_URL='http://s3-ireland.amazonaws.com'
+#S3_URL='http://s3-eu-west-1.amazonaws.com'
 
 # S3 credentials for uploading. If aws-credentials.rb file doesn't exist,
 # the values are read from the environment.
